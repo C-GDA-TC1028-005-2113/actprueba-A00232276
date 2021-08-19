3e263622f9cfe4b614c1fd5d3d834827eefcd2f5
A00232276 <A00232276@itesm.mx>
2021-08-19 15:30:18 +0000

    Envio1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    a= int(input("Dame un número: "))
+    b= int(input("Dame un número: "))
+    suma= a+b
+    resta= a-b
+    multiplicacion= a*b
+    print("Suma:",suma)
+    print("Resta:",resta)
+    print("Multiplicación:",multiplicacion)
 
 
 if __name__ == '__main__':
